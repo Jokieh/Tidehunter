33948cf42d256f7bf02ebe368a40c1a4cb3cfdfc
Isti <1stone.seres@gmail.com>
2021-01-02 17:50:02 +0100

    Update main.lua

--- Tidehunter/main.lua
+++ Tidehunter/main.lua
@@ -6,3 +6,22 @@ class player
      Name = ""; -- Creates a variable for the player's name
      Class = ""; -- Creates a variable for the player's class
   };
+
+  function getHealers ()
+    for i = 1, MAX_RAID_MEMBERS do
+        local currentPlayerInfo = table.pack(GetRaidRosterInfo(i))
+         -- 0 name
+         -- 1 rank
+         -- 2 subgroup
+         -- 3 level
+         -- 4 class
+         -- 5 fileName
+         -- 6 zone
+         -- 7 online
+         -- 8 isDead
+         -- 9 role
+         -- 10 isML
+         -- 11 combatRole
+
+         
+  end
