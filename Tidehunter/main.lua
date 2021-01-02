de39e0562440b6a8c3bdb5d3da822bdfb1771b50
Isti <1stone.seres@gmail.com>
2021-01-02 16:35:23 +0100

    Create main.lua

---
+++ Tidehunter/main.lua
@@ -0,0 +1,8 @@
+class player
+  {
+  public:    
+     GroupOriginal = 0; -- Creates a variable for the player's original group
+     GroupTide = 0; -- Creates a variable for the manatide group
+     Name = ""; -- Creates a variable for the player's name
+     Class = ""; -- Creates a variable for the player's class
+  };
